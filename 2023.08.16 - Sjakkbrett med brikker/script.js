2b45d09552b9591c081ba6d9b2d80dbba0b0e3fb
madsig <madsig@getacademy.no>
2023-08-18 13:08:51 +0200

    Update script.js

--- 2023.08.16 - Sjakkbrett med brikker/script.js
+++ 2023.08.16 - Sjakkbrett med brikker/script.js
@@ -2,13 +2,12 @@ var boardElement = document.getElementById('board');
 const colorSquare = ["lightSquare", "darkSquare"]
 const colorPiece = ["blackPiece", "blackPiece", "whitePiece", "whitePiece"];
 let colorSelect = 0;
-const pieces = [
-    ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
-    ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
-    ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
-    ['', '', '', '', '', '', '', '']
-];
-let pieceRow = 1;
+const pawns = '♟'.repeat(8);
+const pieces = '♜♞♝♛♚♝♞♜';
+const blanks = Array(8).fill('');
+const board = [pieces, pawns, blanks, blanks, blanks, blanks, pawns, pieces];
+
+let pieceRow = 0;
 let pieceSquare = 0;
 let pieceColor = 0;
 
@@ -16,16 +15,14 @@ function showBoard() {
     for (let rows = 0; rows < 8; rows++) {
         for (let squares = 0; squares < 8; squares++) {
             boardElement.innerHTML += /*HTML*/`
-                <div class="${colorSquare[colorSelect]} ${colorPiece[pieceColor]}">${pieces[pieceRow][pieceSquare]}</div>
+                <div class="${colorSquare[colorSelect]} ${colorPiece[pieceColor]}">${board[pieceRow][pieceSquare]}</div>
             `
             colorSelect = (colorSelect + 1) % 2;
             pieceSquare = (pieceSquare + 1) % 8;
         }
         colorSquare.reverse();
         pieceColor = (pieceColor + 1) % 4;
-        if (rows < 2 || rows > 4) {
-            pieceRow = (pieceRow + 1) % 4;
-        }
+        pieceRow++;
     }
 }
 showBoard();
